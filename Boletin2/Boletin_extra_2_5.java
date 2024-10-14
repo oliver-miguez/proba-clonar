6cdce2ccd57a594b2ef9b27527c50609952d7560
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-14 13:05:26 +0200

    Update Boletin_extra_2_5.java

--- Boletin2/Boletin_extra_2_5.java
+++ Boletin2/Boletin_extra_2_5.java
@@ -11,22 +11,26 @@ import java.util.Scanner;
 public class Boletin_extra_2_5 {
     public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
-    float fijo=1300;
+    float fijo;
     float ventas;
     float distancia;
     float hora;
-    
+    float dietas=20;
+
+
+    System.out.println("Cuanto es tu sueldo fijo?");
+    fijo=sc.nextFloat();
     System.out.println("Cuanto ganaste con las ventas?");
     ventas=sc.nextFloat();
     System.out.println("A que distancia estas del trabajo?");
     distancia=sc.nextFloat();
     System.out.println("Cuanto tardas en venir al trabajo?");
     hora=sc.nextFloat();
-    double soldo_bruto=fijo+ventas*0.05+2*distancia+(hora/24)*30;
-    System.out.println("Tu sueldo bruto es "+ soldo_bruto);
+    double soldo_bruto=fijo+ventas*0.05+2*distancia+hora*30+dietas;
+    System.out.println("Tu sueldo bruto es "+ soldo_bruto+" €");
     double irpf=0.18*soldo_bruto;
     float ssocial=36;
-    System.out.println("Tu sueldo liquido es "+(soldo_bruto-(irpf+ssocial)));
+    System.out.println("Tu sueldo liquido es "+(soldo_bruto-(irpf+ssocial))+" €");
     
     
     
