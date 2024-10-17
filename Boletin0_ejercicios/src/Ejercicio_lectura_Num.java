2fcfcac2b619a65fefe0e7942f0116fa27c6a232
Dima <dapariciopereira@danielcastelao.org>
2024-10-17 14:20:31 +0200

    Switch ejercicios mejorado

--- Boletin0_ejercicios/src/Ejercicio_lectura_Num.java
+++ Boletin0_ejercicios/src/Ejercicio_lectura_Num.java
@@ -6,173 +6,258 @@
 
 
 import java.util.Scanner;
+
 public class Ejercicio_lectura_Num {
+
     public static void main(String[] args){
+
         Scanner sc=new Scanner(System.in);
+
         String numero="";
+
         String numero2="";
+
        int numero_d;
+
        int numero_u;
+
        numero_d=9;
+
        numero_u=9;
 
-       /*Preguntar una variable*/
-        System.out.println("Dame una decena del 1-9:");
+
+       /*Preguntar una variable terminada en cero*/
+
+        System.out.println("Dame una decimal terminado en cero del 10-90:");
         numero_d=sc.nextInt();
 
-        /*Preguntar una variable*/
+        /*Preguntar una variable entera*/
+
         System.out.println("Dame un numero entero del 1-9:");
         numero_u=sc.nextInt();
 
     /*Discrimina las distintas opciones de las decenas*/
+
         switch (numero_d){
+
             case 0:
+
                 /*Nos da un caso especial de 0 0*/
+
                 if (numero_d==0){
+
                     if (numero_u==0){
+
                         System.out.println("cero");
                     }
-                }else{
-                    System.out.println(numero);
                 }
-            case 1:
-                /*Me dice los casos especiales de los decimales q empiezan con 10 y terminan con enteros entre 1 y 5*/
+
+            case 10:
+
+                /*Me dice los casos especiales de los decimales que empiezan por uno con enteros del 1 al 5*/
+
                 if (numero_d==1) {
 
                     if (numero_u==1){
+
                         System.out.println("once");
                     }
                     if (numero_u == 2) {
+
                         System.out.println("doce");
                     }
                     if (numero_u == 3) {
+
                         System.out.println("trece");
                     }
                     if (numero_u == 4) {
+
                         System.out.println("catorce");
                     }
                     if (numero_u == 5) {
+
                         System.out.println("quince");
                     }
+
+
+                    /*Decir los casos con decimal empezando por 1 perocon enteros mayores de 5*/
+
                 }else{
+
                     if (numero_d!=0) {
-                        numero = ("diez ");
+
+                        numero = ("diez");
                     }
-                    if (numero_d==0){
+                    else{
+
                         System.out.println(numero2);
                     }
                 }
                 break;
 
-            case 2:
+            case 20:
 
-               numero = ("veinte  ");
+               numero = ("veinte");
+
                break;
 
-            case 3:
+            case 30:
 
-                numero = ("treinta  ");
+                numero = ("treinta");
+
                 break;
 
             case 4:
 
-                numero = ("cuarenta ");
+                numero = ("cuarenta");
+
                 break;
 
-            case 5:
+            case 50:
 
-                numero = ("cincuenta ");
+                numero = ("cincuenta");
+
                 break;
 
-            case 6:
+            case 60:
 
-                numero = ("sesenta   ");
+                numero = ("sesenta");
+
                 break;
 
-            case 7:
+            case 70:
 
-                numero = ("setenta  ");
+                numero = ("setenta");
+
                 break;
 
-            case 8:
+            case 80:
 
-                numero = ("ochenta ");
+                numero = ("ochenta");
+
                 break;
 
-            case 9:
+            case 90:
 
-                numero = ("noventa  ");
+                numero = ("noventa");
+
                 break;
         }
-        //*Discrimina las opciones de enteros*//
+
+       /*Discrimina las opciones de enteros*/
+
         switch (numero_u){
+
+     /*Dos posibilidades, las del decimal que empieza por cero y las del decimal que empieza por por número distinto de cero*/
+
             case 1:
+
                 if (numero_d!=0) {
-                    numero2 = (" y uno");
+
+                    numero2 = ("iuno");
+
                 }else{
+
                     numero2 = ("uno");
                 }
                 break;
+
             case 2:
+
                 if (numero_d!=0) {
-                    numero2 = (" y dos");
+
+                    numero2 = ("idos");
+
                 }else{
+
                     numero2 = ("dos");
                 }
                 break;
+
             case 3:
+
                 if (numero_d!=0) {
-                    numero2 = (" y tres");
+
+                    numero2 = ("itres");
+
                 }else{
+
                     numero2 = ("tres");
                 }
                 break;
+
             case 4:
+
                 if (numero_d!=0) {
-                    numero2 = (" y cuatro");
+
+                    numero2 = ("icuatro");
+
                 }else{
+
                     numero2 = ("cuatro");
                 }
                 break;
+
             case 5:
+
                 if (numero_d!=0) {
-                    numero2 = (" y cinco");
+
+                    numero2 = ("icinco");
+
                 }else{
+
                     numero2 = ("cinco");
                 }
                 break;
+
             case 6:
+
                 if (numero_d!=0) {
-                    numero2 = (" y seis");
+
+                    numero2 = ("iseis");
+
                 }else {
+
                     numero2 = (" seis");
                 }
                 break;
+
             case 7:
+
                 if (numero_d!=0) {
-                    numero2 = (" y siete");
+
+                    numero2 = ("isiete");
+
                 }else {
+
                     numero2 = (" siete");
                 }
                 break;
+
             case 8:
+
                 if (numero_d!=0) {
-                    numero2 = (" y ocho");
+
+                    numero2 = ("iocho");
+
                 }else {
+
                     numero2 = (" ocho");
                 }
                 break;
+
             case 9:
+
                 if (numero_d!=0) {
-                    numero2 = (" y nueve");
+
+                    numero2 = ("inueve");
+
                 }else {
+
                     numero2 = (" nueve");
                 }
                 break;
         }
         System.out.println(numero+numero2);
-        
-        
-        
     }
 }
