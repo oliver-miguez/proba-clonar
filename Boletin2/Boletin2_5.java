f9cb504d6ff0a224c16b8b21417997ebe762d1cf
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-19 17:01:39 +0200

    Update Boletin2_5.java

--- Boletin2/Boletin2_5.java
+++ Boletin2/Boletin2_5.java
@@ -11,10 +11,13 @@ import java.util.Scanner;
 public class Boletin2_5 {
     public static void main(String[] args){
     Scanner s= new Scanner(System.in);
+        /*Preguntar variable int por consola*/
     System.out.println("Dime en que milla te encuentras?:");
     long milla;
     milla=s.nextLong();
+        /*Operacion para el calculo de millas a metros*/
     double metro=(milla*1.852);
+        /*salida del resultado de la operacion*/
     System.out.println("Entonces son "+ metro +" kilometros");
  s.close();
     }
