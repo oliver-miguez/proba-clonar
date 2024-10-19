9cb34315cb1e870e12a0f57e6b5c9aefee37dc24
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-19 17:42:16 +0200

    Update Boletin2_3.java

--- Boletin2/Boletin2_3.java
+++ Boletin2/Boletin2_3.java
@@ -4,21 +4,25 @@
  */
 package com.danielcastelao.newproyect.Proyectos.Prog;
 import java.util.Scanner;
-/**
- *
- * @author dam
+/**Cambiar de dolares a euros
+ *@version 1.1
+ * @author Dima Aparicio
  */
 public class Boletin2_3 {
    public static void main(String[] args){
        Scanner s=new Scanner(System.in);
     System.out.println("Dimecual es tu nombre?:");
     String nombre;
+      /*Variable string de identidad nombre*/
      nombre = s.nextLine();
     System.out.println("Hola "+ nombre);
     System.out.println("Cuantos euros quieres cambiar a dólares?:");
+      /*variable int dinero*/
      int dinero;
      dinero=s.nextInt();
+      /*Operacion con variables dadas*/
      double dolares = dinero*(1.11);
+      /*Salida de resultados*/
      System.out.println("Tienes "+ dolares  +" $");
  s.close();
     
