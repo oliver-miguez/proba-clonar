115ae87a7cf8d79429a8d4e1284f44577cb56a14
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-19 16:58:13 +0200

    Update Boletin_extra_2_4.java

--- Boletin2/Boletin_extra_2_4.java
+++ Boletin2/Boletin_extra_2_4.java
@@ -6,16 +6,18 @@ package com.danielcastelao.newproyect.Proyectos.Prog;
 
 import java.util.Scanner;
 
-/**
- *
- * @author dam
+/**Da cambio desglosado desoues de escribirle por consola la cantidad que quieres separa
+ *@version 1.1
+ * @author Dima Aparicio
  */
 public class Boletin_extra_2_4 {
     public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
+        /*Variable int de la cantidad total*/
     int dinero;
     System.out.println("Cuanto es el dinero que necesitas: ");
     dinero=sc.nextInt();
+        /*Operaciones necesarias para que nos quedemos con el numero de centimos final*/
     int n1=dinero/100;
     int dinero1=dinero-(n1*100);
     int n2=dinero1/20;
@@ -23,6 +25,7 @@ public class Boletin_extra_2_4 {
     int n3=dinero2/5;
     int dinero3=dinero2-(n3*5);
     int m=dinero3/1;
+        /*Salida del resultado por partes*/
     System.out.println("Necesitamos "+n1+" billetes de 100€ "+" , "+n2+" billetes de 20€"+" , "+n3+" billetes de 5€ "+" , "+m+" monedas de 1€");
     
     
