c46167ce5895b837c0b578340239ecee60c3bc37
Dimitri2004 <dapariciopereira@danielcastelao.org>
2024-10-19 17:16:13 +0200

    Update Boletin2_4.java

--- Boletin2/Boletin2_4.java
+++ Boletin2/Boletin2_4.java
@@ -13,10 +13,13 @@ public class Boletin2_4 {
     Scanner s=new Scanner(System.in);
     System.out.println("Dame tu primera variable: ");
     double a;
+        /*Variable double del primer numero*/
     a=s.nextLong();
     System.out.println("Dame tu siguiente variable:");
     double b;
+        /*variable double del segundo numero*/
     b=s.nextLong();
+        /*salida de las operaciones entre las variables dadas*/
     System.out.println("La sume de estos es:"+(a+b));
     System.out.println("La resta de los dos es:"+(a-b));
     System.out.println("El producto entre ellos es: "+a*b);
