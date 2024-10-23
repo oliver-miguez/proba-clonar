d0247f24c69706d11780a4d42e6b6c943de130ff
Dima <dapariciopereira@danielcastelao.org>
2024-10-23 09:01:19 +0200

    Ejercicio de Multiplos

---
+++ Boletin 4/src/Multiplos_4.java
@@ -0,0 +1,28 @@
+public class Multiplos_4 {
+    public static void main(String[] args){
+/*Bucle que repite procesos a partir de unas condiciones */
+        for(int a=1; a<=100;a++){
+            /*Salida de multiplos de 7*/
+            System.out.println(a*7);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
